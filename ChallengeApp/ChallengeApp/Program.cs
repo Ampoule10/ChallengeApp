32fe035adddeb452e9a87e0565859f222c9f845f
Ampoule10 <ampoule10@gmail.com>
2023-02-28 20:17:09 +0100

    Zadanie dzień 4.

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+Console.WriteLine(name);
+string gender = "female";
+Console.WriteLine(gender);
+int age = 33;
+Console.WriteLine(age);
+
+if (gender == "female" && age < 33)
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+
+}
+else
+    Console.WriteLine("To nie jest kobieta ponizej 30 lat");
+
+if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else
+    Console.WriteLine("To nie jest Ewa, lat 33");
+
+if (age < 18 && gender == "male")
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");
+}
+else
+    Console.WriteLine("To nie jest niepelnoletni mezczyzna");
